e46072c9795abfc7e70ea18628086c92bad53ca5
Yeongyeong <dhwm0710@naver.com>
2025-03-07 00:03:38 +0900

    homework

--- algorithm/3.06/단순 2진 암호코드.py
+++ algorithm/3.06/단순 2진 암호코드.py
@@ -11,23 +11,27 @@ for tc in range(1, T + 1):
         for j in range(M - 1, -1, -1):  # 미로의 시작위치를 찾듯 1으로 끝나는 가장 첫번째 좌표를 찾음
             if code_arr[i][j]:
                 x, y = i, j
-                found1 = True
+                found1 = True   # 처음 1을 찾게되면 True로 변경하고 이중포문 break
                 break
         if found1:
             break
 
     find_code = [''] * 8
-    print(x,y)
+
     for k in range(8):
         for l in range(7):
-            find_code[k] += str(code_arr[x + k][y - 55 + l])
-            # 줄이 안바뀌고 있는것같음,,,,
-    print(find_code)
+            find_code[k] += str(code_arr[x][y - 55 + l + (7 * k)])
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(8):  # 맨처음에 만든 코드배열과 비교하여 일치하는 인덱스로 대체
+        for j in range(10):
             if find_code[i] == codes[j]:
                 find_code[i] = j
 
-    print(find_code)
+    right_code = ((find_code[0] + find_code[2] + find_code[4] + find_code[6]) * 3) + \
+                 (find_code[1] + find_code[3] + find_code[5]) + find_code[7]
 
+    if right_code % 10 == 0 :   # 주어진 공식에 해당하는지 확인(올바른코드 여부)
+        result = sum(find_code)
+        print(f'#{tc} {result}')
+    else:
+        print(f'#{tc} 0')
