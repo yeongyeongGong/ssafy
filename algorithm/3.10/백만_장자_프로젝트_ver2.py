41cd6125bdbaaa746d060bd487bdc5e3fecc6be2
공연경 <dhwm0710@naver.com>
2025-03-11 18:02:00 +0900

    dafsd

---
+++ algorithm/3.10/백만_장자_프로젝트_ver2.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open('백만_장자_프로젝트.txt')
+
+T = int(input())    # 테스트 케이스
+
--- algorithm/3.10/자기_방으로_돌아가기.py
+++ algorithm/3.10/자기_방으로_돌아가기.py
@@ -2,9 +2,18 @@ T = int(input())  # 테스트케이스
 
 for tc in range(1, T + 1):
     N = int(input())  # 돌아가야할 학생 수
-    corridor = [0] * 200    # 복도
+    corridor = [0] * 201    # 복도
     for i in range(N):
-        current, move = map(int, input().split())  # current : 현재 위치, move: 이동해야할 방 위치
+        s, e = map(int, input().split())  # s : 현재 위치, e: 이동해야할 방 위치
 
+        s = (s + 1) // 2
+        e = (e + 1) // 2
 
+        if s > e:
+            for i in range(e, s+1):
+                corridor[i] += 1
+        elif s < e:
+            for i in range(s, e + 1):
+                corridor[i] += 1
 
+    print(f'#{tc} {max(corridor)}')
